8c6e52e6812829a645221696680e09a29cf3234b
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-28 09:04:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(d)
 end
 
 def summon_captain_planet(d)
-  d.map { |v| v.title() + '!' }
+  d.map { |v| v.capitalize() + '!' }
 end
 
 def long_planeteer_calls# code an argument here
