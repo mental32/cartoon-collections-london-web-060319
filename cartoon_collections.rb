1752d1e4dff81ad2e605404b73e915e032cdce9a
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-28 09:04:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(d)
   d.map { |v| v.capitalize() + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(d)
+  
 end
 
 def find_the_cheese# code an argument here
